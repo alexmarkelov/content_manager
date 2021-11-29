2d1872c4ca64994d98c2a2e18213063cb3d50b81
alexmarkelov <markellexa@mail.ru>
2021-11-29 13:12:41 +0700

    Model for Episode and it's test were added.

--- content_aggregator/podcasts/models.py
+++ content_aggregator/podcasts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Episode(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    pub_date = models.DateTimeField()
+    link = models.URLField()
+    image = models.URLField()
+    podcast_name = models.CharField(max_length=150)
+    guid = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.podcast_name}: {self.title}"
--- content_aggregator/podcasts/tests.py
+++ content_aggregator/podcasts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Episode
 
-# Create your tests here.
+# test data
+test_title = "Test title"
+test_description = "Some information about episode"
+test_link = "https\\coubs.com"
+test_image = "http:\\www.w3.org\\2000\\svg"
+test_podcast_name = "My test podcast"
+test_guid = "22345200-abe8-4f60-90c8-0d43c5f6c0f6"
+
+class PodCastsTests(TestCase):
+    def setUp(self) -> None:
+        self.episode = Episode.objects.create(
+            title=test_title,
+            description=test_description,
+            pub_date=timezone.now(),
+            link=test_link,
+            image=test_image,
+            podcast_name=test_podcast_name,
+            guid=test_guid,
+        )
+
+    def test_episode_content(self):
+        self.assertEqual(self.episode.description, test_description)
+        self.assertEqual(self.episode.link, test_link)
+        self.assertEqual(self.episode.guid, test_guid)
+
+    def test_episode_str_repr(self):
+        self.assertEqual(str(self.episode), test_podcast_name + ': ' + test_title)
